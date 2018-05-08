7444b9f54206eeb5e7340707399c1d74e09f9b85
osobo <s.gryazev@dodopizza.com>
2018-05-08 09:34:42 +0300

    Initial Commit

---
+++ Swift2VkClient/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift2VkClient
+//
+//  Created by yosh on 08.05.2018.
+//  Copyright © 2018 yosh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
