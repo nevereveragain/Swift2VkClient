f76a84391a8211abb10633c1531369d018aae4e3
osobo <yoshgs@gmail.com>
2018-06-15 20:51:58 +0300

    lesson8

--- Swift2VkClient/Controllers/Friends Scene Controllers/FriendsSceneController.swift
+++ Swift2VkClient/Controllers/Friends Scene Controllers/FriendsSceneController.swift
@@ -11,25 +11,66 @@ import SwiftKeychainWrapper
 import RealmSwift
 
 class FriendsSceneController : UITableViewController {
-    var friendsList = [
-        "Ivan Ivanov",
-        "Darth Vader",
-        "Tom and Jerry"
-    ]
+    //    var friendsList = [
+    //        "Ivan Ivanov",
+    //        "Darth Vader",
+    //        "Tom and Jerry"
+    //    ]
     
     var service: VKService?
     var friends = [FriendWithPhoto]()
-    
+    var friendsToUpdate: Results<FriendWithPhoto>!
+    var notificationToken: NotificationToken?
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         loadData()
         
     }
+    func loadDataFromRealmViaNotifications () {
+        guard let realm = try? Realm() else { return }
+        friendsToUpdate = realm.objects(FriendWithPhoto.self)
+        notificationToken = friendsToUpdate.observe { changes in
+            switch changes {
+            case .initial:
+                self.tableView.reloadData()
+            case .update(_, let deletions, let insertions, let modifications):
+                self.tableView.beginUpdates()
+                self.tableView.insertRows(at: insertions.map({ IndexPath(row: $0, section: 0) }), with: .automatic)
+                self.tableView.deleteRows(at: deletions.map({ IndexPath(row: $0, section: 0) }), with: .automatic)
+                self.tableView.reloadRows(at: modifications.map({ IndexPath(row: $0, section: 0) }), with: .automatic)
+                self.tableView.endUpdates()
+            case .error(let error):
+                print(error)
+            }
+        }
+    }
+    
+    //    func pairTableAndRealm() {
+    //        guard let realm = try? Realm() else { return }
+    //        cities = realm.objects(City.self)
+    //
+    //        notificationToken = cities.observe { changes in
+    //            switch changes {
+    //            case .initial:
+    //                self.tableView.reloadData()
+    //            case .update(_, let deletions, let insertions, let modifications):
+    //                self.tableView.beginUpdates()
+    //                self.tableView.insertRows(at: insertions.map({ IndexPath(row: $0, section: 0) }), with: .automatic)
+    //                self.tableView.deleteRows(at: deletions.map({ IndexPath(row: $0, section: 0) }), with: .automatic)
+    //                self.tableView.reloadRows(at: modifications.map({ IndexPath(row: $0, section: 0) }), with: .automatic)
+    //                self.tableView.endUpdates()
+    //            case .error(let error):
+    //                print(error)
+    //            }
+    //        }
+    
     
     //TODO: Refactoring
     func loadData() {
         guard let token: String = KeychainWrapper.standard.string(forKey: "userToken") else { return }
+        loadDataFromRealmViaNotifications()
         service = VKService (token: token)
         service?.getFriends(completion: { (friends) in
             if let friends = friends {
@@ -45,13 +86,13 @@ class FriendsSceneController : UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return friends.count
+        return friendsToUpdate.count
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let friendController = segue.destination as? FriendSceneCollectionController {
             if let indexPath = tableView.indexPathForSelectedRow {
-                let friend = friends[indexPath.row]
+                let friend = friendsToUpdate[indexPath.row]
                 friendController.title = "\(friend.firstName)" + " " + "\(friend.lastName)"
                 friendController.friendAvatar = friend
             }
@@ -62,7 +103,7 @@ class FriendsSceneController : UITableViewController {
         UITableViewCell {
             // получаем ячейку из пула
             let cell = tableView.dequeueReusableCell(withIdentifier: "TableToCollection", for: indexPath) as! FriendsSceneCellController
-            let friendCell = friends[indexPath.row]
+            let friendCell = friendsToUpdate[indexPath.row]
             cell.setFriendCell(friendCell)
             return cell
             
--- Swift2VkClient/Controllers/Groups Scene Controllers/UserGroupsListController.swift
+++ Swift2VkClient/Controllers/Groups Scene Controllers/UserGroupsListController.swift
@@ -8,33 +8,51 @@
 
 import UIKit
 import SwiftKeychainWrapper
+import RealmSwift
 
 class UserGroupListController: UITableViewController {
     
     var groups = [Groups]()
     var service: VKService?
+    var groupsToUpdate: Results<Groups>!
+    var notificationToken: NotificationToken?
     
     @IBAction func addGroup(segue: UIStoryboardSegue) {
         guard let allGroupsController = segue.source as? AllGroupsListController else {
             return
         }
         
-//        if let indexPath = allGroupsController.tableView.indexPathForSelectedRow {
-//            let groupToAdd = allGroupsController.filteredData[indexPath.row]
-//            if !groupsList.contains(groupToAdd.name) {
-//                groupsList.append(groupToAdd.name)
-//                tableView.reloadData()
-//            }
-//        }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         loadData()
+        
     }
+    
+    func loadDataFromRealmViaNotifications () {
+        guard let realm = try? Realm() else { return }
+        groupsToUpdate = realm.objects(Groups.self)
+        notificationToken = groupsToUpdate.observe { changes in
+            switch changes {
+            case .initial:
+                self.tableView.reloadData()
+            case .update(_, let deletions, let insertions, let modifications):
+                self.tableView.beginUpdates()
+                self.tableView.insertRows(at: insertions.map({ IndexPath(row: $0, section: 0) }), with: .automatic)
+                self.tableView.deleteRows(at: deletions.map({ IndexPath(row: $0, section: 0) }), with: .automatic)
+                self.tableView.reloadRows(at: modifications.map({ IndexPath(row: $0, section: 0) }), with: .automatic)
+                self.tableView.endUpdates()
+            case .error(let error):
+                print(error)
+            }
+        }
+    }
+    
     
     func loadData() {
         guard let token: String = KeychainWrapper.standard.string(forKey: "userToken") else { return }
+        loadDataFromRealmViaNotifications()
         service = VKService (token: token)
         service?.getGroups(completion: { (groups) in
             if let groups = groups {
@@ -49,7 +67,7 @@ class UserGroupListController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return groups.count
+        return groupsToUpdate.count
     }
     
     
@@ -58,20 +76,20 @@ class UserGroupListController: UITableViewController {
             // получаем ячейку из пула
             let cell = tableView.dequeueReusableCell(withIdentifier: "GroupCell", for: indexPath) as! UserGroupsCellController
             // получаем имя для конкретной строки
-            let group = groups[indexPath.row]
+            let group = groupsToUpdate[indexPath.row]
             // устанавливаем имя в надпись ячейки
             cell.setGroupCell(group)
             return cell
     }
     
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle,
-                            forRowAt indexPath: IndexPath) {
-        // если была нажата кнопка удалить
-        if editingStyle == .delete {
-            // мы удаляем группу из массива
-            groups.remove(at: indexPath.row)
-            // и удаляем строку из таблицы
-            tableView.deleteRows(at: [indexPath], with: .left)
-        }
-    }
+    //    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle,
+    //                            forRowAt indexPath: IndexPath) {
+    // если была нажата кнопка удалить
+    //        if editingStyle == .delete {
+    //            // мы удаляем группу из массива
+    //            groups.remove(at: indexPath.row)
+    //            // и удаляем строку из таблицы
+    //            tableView.deleteRows(at: [indexPath], with: .left)
+    //        }
 }
+
